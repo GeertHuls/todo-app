a17b4c4b0b894b37ca2f4961e328bcf6fb2874b0
Geert Huls <geert.huls82@gmail.com>
2016-09-13 13:04:10 +0200

    Copied entire app to android startup file

---
+++ TodoAppFull/TaskList.js
@@ -0,0 +1,99 @@
+import React from 'react';
+
+import {
+  View,
+  StyleSheet,
+  ListView,
+  TouchableHighlight,
+  Text
+} from 'react-native';
+
+import TaskRow from './TaskRow';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    backgroundColor: '#F7F7F7',
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  button: {
+    height: 60,
+    borderColor: '#05A5D1',
+    borderWidth: 2,
+    backgroundColor: '#333',
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#FAFAFA',
+    fontSize: 20,
+    fontWeight: '600'
+  }
+});
+
+class TaskList extends React.Component{
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.renderRow = this.renderRow.bind(this);
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.state = {
+      dataSource: ds.cloneWithRows(props.todos)
+    };
+  }
+
+  //Happens right after the set state command in the onAdd function
+  //in the main component. Recalculate the dataSource
+  //with the latest data.
+  componentWillReceiveProps(nextProps) {
+    const dataSource = this
+      .state
+      .dataSource
+      .cloneWithRows(nextProps.todos);
+
+      this.setState({ dataSource });
+  }
+
+  renderRow(todo) {
+    return (
+      <TaskRow
+        onDone={this.props.onDone}
+        todo={todo} />
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ListView
+          key={this.props.todos}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow} />
+
+        <TouchableHighlight
+          onPress={this.props.onAddStartedNav}
+          style={styles.button}>
+            <Text style={styles.buttonText}>
+              Add one
+            </Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+TaskList.propTypes = {
+  onAddStartedNav: React.PropTypes.func.isRequired,
+  onDone: React.PropTypes.func.isRequired,
+  todos: React.PropTypes
+    .arrayOf(React.PropTypes.object).isRequired
+};
+
+export default TaskList;
---
+++ TodoAppFull/TodoApp.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import {
+  Navigator
+} from 'react-native';
+
+import TaskList from './TaskList';
+import TaskForm from './TaskForm';
+
+class TodoApp extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.referenceNav = this.referenceNav.bind(this);
+    this.onAddStartedNav = this.onAddStartedNav.bind(this);
+    this.renderScene = this.renderScene.bind(this);
+
+    this.onCancel = this.onCancel.bind(this);
+    this.onAdd = this.onAdd.bind(this);
+
+    this.onDone = this.onDone.bind(this);
+
+    this.state = {
+      todos: [
+        {
+          task:'Learn react native'
+        },
+        {
+            task: 'Learn Redux'
+        }
+      ]
+    };
+  }
+
+  onAddStartedNav() {
+    this.nav.push({
+      name:'taskform'
+    });
+  }
+
+  onCancel() {
+    //Hide the current view
+    //and revert to the previous view.
+    this.nav.pop();
+  }
+
+  onAdd(task) {
+    this.state.todos.push({ task });
+
+    this.setState({
+      todos: this.state.todos
+    });
+    this.nav.pop();
+  }
+
+  onDone(todo) {
+    const filteredTodos =
+      this.state.todos.filter((filterTodo) => {
+        return filterTodo !== todo;
+      });
+
+      this.setState({ todos: filteredTodos})
+  }
+
+  renderScene(route, nav) {
+    switch (route.name) {
+      case 'taskform':
+        return (
+          <TaskForm
+            onAdd={this.onAdd}
+            onCancel={this.onCancel}
+            />
+        );
+      default:
+        return (
+          <TaskList
+            onAddStartedNav={this.onAddStartedNav}
+            onDone={this.onDone}
+            todos={this.state.todos}
+            />
+        );
+    }
+  }
+
+  configureScene() {
+    return Navigator.SceneConfigs.FloatFromBottom;
+  }
+
+  referenceNav(nav) {
+    this.nav = nav;
+  }
+
+  render() {
+      return (
+        <Navigator
+          configureScene={this.configureScene}
+          ref={this.referenceNav}
+          initialRoute={{
+              name: 'tasklist',
+              index: 0
+          }}
+          renderScene={this.renderScene} />
+      );
+  }
+}
+
+export default TodoApp;
