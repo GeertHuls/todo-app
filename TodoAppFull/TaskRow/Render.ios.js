e1785466b6f78903890d0d4ebf83e8cb2a24d413
Geert Huls <geert.huls82@gmail.com>
2016-09-14 22:48:07 +0200

    Better styling for iOS app (untested)

--- TodoAppFull/TaskRow/Render.ios.js
+++ TodoAppFull/TaskRow/Render.ios.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 import {
   Text,
-  View
+  View,
+  StyleSheet
 } from 'react-native';
 
 import Swipout from react-native-swipeout;
 
-export default function render(styles) {
+const localStyle = StyleSheet.create({
+  row: {
+    marginBottom: 0,
+    marginLeft: 0,
+    marginRight: 0
+  },
+  container: {
+    marginBottom: 20
+  }
+});
 
+export default function render(baseStyle) {
   const buttons = [
     {
       text: 'Done',
@@ -19,14 +30,16 @@ export default function render(styles) {
   ]
 
   return (
-    <SwipeOut
-      backgroundColor='#fff'
-      right={buttons}>
-      <View style={styles.container}>
-        <Text style={styles.label}>
-          ios: {this.props.todo.task}
-        </Text>
-      </View>
-    </SwipeOut>
+    <View style={localStyle.container}>
+      <SwipeOut
+        backgroundColor='#fff'
+        right={buttons}>
+        <View style={[baseStyle.container, localStyle.row]}>
+          <Text style={baseStyle.label}>
+            ios: {this.props.todo.task}
+          </Text>
+        </View>
+      </SwipeOut>
+    </View>
   );
 }
