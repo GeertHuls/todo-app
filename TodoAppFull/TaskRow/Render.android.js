18e711c0216f7296cf27325b24e63f456436ae16
Geert Huls <geert.huls82@gmail.com>
2016-09-14 22:54:47 +0200

    Better styling for android app

--- TodoAppFull/TaskRow/Render.android.js
+++ TodoAppFull/TaskRow/Render.android.js
@@ -3,19 +3,29 @@ import React from 'react';
 import {
   Text,
   View,
-  TouchableHighlight
+  TouchableHighlight,
+  Image,
+  StyleSheet
 } from 'react-native';
+
+const localStyle = StyleSheet.create({
+  doneButton: {
+    borderRadius: 5,
+    padding: 5
+  }
+});
 
 export default function render(styles) {
   return (
     <View style={styles.container}>
       <Text style={styles.label}>
-        android: {this.props.todo.task}
+        {this.props.todo.task}
       </Text>
       <TouchableHighlight
         onPress={this.onDonePressed}
-        style={styles.doneButton}>
-          <Text>Done</Text>
+        style={localStyle.doneButton}
+        underlayColor="#ddd">
+          <Image source={require('../images/done.png')} />
       </TouchableHighlight>
     </View>
   );
